2d406a876d171d3fce39bb26f46873dff503046e
Sima <sima.dhimal97@gmail.com>
2025-03-19 11:38:49 +0100

    Now List can be deleted

--- webapp.py
+++ webapp.py
@@ -10,12 +10,16 @@ def add_todo():
   todo = st.session_state["new_todo"].strip()
   todos.append(todo + "\n")
   functions.write_todos(todos)
-  st.session_state["new_todo"] = "Add new todo..."
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key = "new_todo")
 
-for todo in todos:
-  st.checkbox(todo)
+for index, todo in enumerate(todos):
+  checkbox = st.checkbox(todo, key=todo)
+  if checkbox:
+    todos.pop(index)
+    functions.write_todos(todos)
+    del st.session_state[todo]
+    st.rerun()
 
-
+st.session_state
